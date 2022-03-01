c34e68a1d7d402245a7cacfda7e25a373c4713ba
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-02-28 20:19:16 -0500

    GUI implemented

--- main.py
+++ main.py
@@ -1,77 +1,198 @@
+# from tkinter import *
+from tkinter import Tk, Menu, mainloop, messagebox, ttk, Toplevel, Label, Entry, font, Button, Frame, IntVar, Radiobutton, Scrollbar, Canvas, StringVar, filedialog
+from tkinter.ttk import Progressbar
 from pytube import YouTube
-from pytube.cli import on_progress
+from threading import Thread
+from yt import convert_seconds, download_video
 
-def convert_seconds(seconds):
-    units = ["seconds", "minutes", "hours", "days"]
-    temp = None
+class App(Tk):
+    def __init__(self):
+        super().__init__()
 
-    if seconds < 0:
-        return None
-    duration = [0, 0, 0, 0]
+        self.title('Youtube Downloader')
+        window_width, window_height, position_right, position_top = self.get_geometry_data()
+        self.geometry(f'{window_width}x{window_height}+{position_right}+{position_top}')
+        self.window_width = window_width
+        self.window_height = window_height
 
-   # days
-    temp = seconds // 86400 # 86400 seconds in a day
-    duration[3] = temp
-    # hours
-    temp = (seconds % 86400) // 3600 # 3600 seconds in an hour
-    duration[2] = temp
-    # minutes
-    temp = (seconds % 3600) // 60 # 60 seconds in a minute
-    duration[1] = temp
-    # seconds
-    temp = (seconds % 60)
-    duration[0] = temp
+        self.menubar = Menu(self, background='#dddddd', foreground='black', activebackground='white', activeforeground='black')  
 
-    duration_to_string = ""
-    for i in range(len(duration)-1, -1, -1):
-        if duration[i] == 1:
-            duration_to_string += str(duration[i]) + " " + units[i][:-1] + ", "
-        elif duration[i] != 0:
-            duration_to_string += str(duration[i]) + " " + units[i] + ", "
-    return duration_to_string[:-2]
+        file = Menu(self.menubar, tearoff=0, background='#dddddd')  
+        file.add_command(label="New", command=self.new)
+        file.add_separator()  
+        file.add_command(label="Exit", command=self.quit)  
+        self.menubar.add_cascade(label="File", menu=file)  
+        help = Menu(self.menubar, tearoff=0, background='#dddddd')
+        help.add_command(label="About", command=self.about)  
+        self.menubar.add_cascade(label="Help", menu=help)  
+        self.config(menu=self.menubar)
+
+        self.progressbar = Progressbar(
+            self, 
+            orient='horizontal',
+            length=280
+        )
+        self.progressbar.pack()
+        self.type = StringVar()
+
+        self.main_frame = Frame(
+            self, 
+            bg='#A8B9BF', 
+            width=self.window_width,
+            height=self.window_height
+        )
+        self.main_frame.pack(expand=True, fill="both")
 
 
-#Asking for all the video links
-n = int(input("Enter the number of youtube videos to download:   "))
-links=[]
-print("\nEnter all the links one per line:")
+        # self.links = []
+        self.current_link = None
+        self.option = IntVar()
+        self.ext =""
+        self.categories = {}
+        self.yt_title = ""
+        self.duration = ""
+        self.current_percentage = 0
 
-for i in range(0,n):
-    temp = input()
-    links.append(temp)
+    def get_geometry_data(self):
+        # get the screen size of your computer
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        # calculate a suitable size for your window
+        window_width = int(screen_width * 0.5)
+        window_height = int(screen_height * 0.7)
+        # Get the window position from the top dynamically
+        position_top = int((screen_height- window_height)/4)
+        position_right = int((screen_width - window_width)/2)
 
-#Showing all details for videos and downloading them one by one
-for i in range(0,n):
-    link = links[i]
-    # yt = YouTube(link)
-    yt=YouTube(link,on_progress_callback=on_progress)
-    print("\nDetails for Video",i+1,"\n")
-    print("Title of video:   ",yt.title)
-    print("Number of views:  ",yt.views)
-    print("Length of video:  ", convert_seconds(yt.length), " (", yt.length, "seconds)")
+        return window_width, window_height, position_right, position_top 
 
-    temp = yt.streams #.filter(progressive=True)
-    # print(temp)
-    stream = str(temp)
-    stream = stream[1:]
-    stream = stream[:-1]
-    streamlist = stream.split(", ")
 
-    print("\nAll available options for downloads:\n")
-    for i in range(0,len(streamlist)):
-        st = streamlist[i].split(" ")
-        # print(i+1,") ",st[1]," and ",st[3],sep='')
-        for feature in st:
-            print(feature, end=" ")
-        print()
+    def about(self):
+        self.mssg_box = messagebox.showinfo('Youtube Downloader',
+            'Youtube Downloader v1.0.0\n\nCreated by: Jack Christopher\n\nCopyright (c) 2022')
+
+    def new(self):
+        self.add_link_window = Toplevel(self)
+        self.add_link_window.title("Enter Link")
+        self.add_link_window.geometry("500x150")
+        Label(self.add_link_window, text ="Enter the Youtube Link: ").pack()
+        link = Entry(self.add_link_window, width = 50, font=font.Font(family="Courier", size=10) )
+        link.pack()
+        Radiobutton(self.add_link_window, text="Solo Audio", variable=self.type, value="audio").pack()
+        Radiobutton(self.add_link_window, text="Solo Video", variable=self.type, value="video").pack()
+        Radiobutton(self.add_link_window, text="Audio and Video", variable=self.type, value="audio_video").pack()
+        Button(self.add_link_window, text = "Add Link", command= lambda: self.add_link(link)).pack()
     
-    tag = int(input("\nEnter the itag of your preferred stream to download:   "))
-    ys = yt.streams.get_by_itag(tag)
-    print("\nDownloading...")
-    output_path = input("Enter your preferred output path:   ")
-    filename = input("Enter the file name: (type 'default' if you want to use the default name)   ")
-    if filename == "default":
-        filename = yt.title
-    ys.download(output_path=output_path, filename=filename)
-    print("\nDownload completed!!")
-    print()
+    def add_link(self, link):
+        link = link.get()
+        if link == "":
+            return
+        self.add_link_window.destroy()
+        self.main_frame.destroy()
+        self.main_frame = Frame(
+            self, 
+            bg='#A8B9BF', 
+            width=self.window_width,
+            height=self.window_height
+        )
+        self.main_frame.pack(expand=True, fill="both", side="left")
+
+
+        self.yt, self.yt_title, self.duration, self.categories = self.get_stream_data(link)
+        Label(
+            self.main_frame, 
+            bg='#A8B9BF',
+            text= self.yt_title + self.duration 
+        ).pack()
+        
+        Label(
+            self.main_frame, 
+            bg='#A8B9BF',
+            text=(self.type.get()).replace("_", " and ").capitalize()+":"
+        ).pack()
+
+        for stream in self.categories[self.type.get()]:
+            text = ""
+            if self.type.get() == "audio":
+                text = "type: " + stream['mime_type'] + ", abr: " + stream['abr'] + ", audio codec: " + stream['acodec']
+            elif self.type.get() == "video":
+                text = "type: " + stream['mime_type'] + ", resolution: " + stream['res'] + ", fps: " + stream['fps'] + ", video codec: " + stream['vcodec']
+            elif self.type.get() == "audio_video":
+                text = "type: " + stream['mime_type'] + ", resolution: " + stream['res'] + ", fps: " + stream['fps'] + ", video codec: " + stream['vcodec'] + ", audio codec: " + stream['acodec']
+
+            Radiobutton(
+                self.main_frame, 
+                bg='#A8B9BF',
+                text=text, 
+                variable=self.option, 
+                value=stream['itag'], 
+                # command= self.download with lambda function and thread
+                command= lambda: Thread(target=self.download).start()
+
+            ).pack()
+        
+    def download(self):
+        # ask for directory to save to
+        self.output_path = filedialog.askdirectory()
+        self.filename = ""
+        itag = self.option.get()
+        for category in self.categories:
+            for stream in self.categories[category]:
+                if int(stream['itag']) == itag:
+                    self.ext = stream['mime_type'].split('/')[1]
+
+        download_video(self.yt, itag, self.output_path, self.filename, self.ext)
+
+
+
+    def progress_function(self, stream, chunk, bytes_remaining):
+        size = stream.filesize
+        p = round((1- (float(bytes_remaining) / float(size))) * float(100), 1)
+        # print (str(p)+'% (', bytes_remaining , ' bytes remaining)')
+        self.progressbar.step(p - self.current_percentage)
+        self.current_percentage = p
+        
+
+    def completed_function(self, stream, filepath):
+        self.download_complete_window = Toplevel(self)
+        self.download_complete_window.title("Download Complete")
+        self.download_complete_window.geometry("200x100")
+        Label(self.download_complete_window, text ="Download Complete!").pack()
+        Button(self.download_complete_window, text = "OK", command= self.download_complete_window.destroy).pack()
+        self.current_percentage = 0
+        # print("\nDownload Complete")
+
+    def get_stream_data(self, link):
+        # Default progress bar
+        # yt=YouTube(link, on_progress_callback=on_progress)
+        yt=YouTube(link, on_progress_callback=self.progress_function, on_complete_callback=self.completed_function)
+        title = "Title:   " + yt.title + "\n"
+        duration = "Length:  " + convert_seconds(yt.length) + " ( " + str(yt.length) + " seconds )\n"
+
+
+        stream_list = yt.streams
+        # remove brackets from the stream_list and split it into a list
+        stream_list = str(stream_list)[1:-1].split(", ")
+
+        categories = { "audio": [], "video": [], "audio_video":[] }
+
+        current_category = None
+        for i in range(len(stream_list)):
+            # remove initial and final brackets from the stream_list
+            temp = stream_list[i][1:-1]
+            if "acodec" in temp and "vcodec" in temp:
+                current_category = "audio_video"
+            elif "acodec" in temp:
+                current_category = "audio"
+            elif "vcodec" in temp:
+                current_category = "video"
+            temp = temp.replace("\"", "").split(" ")[1:]
+            temp = {k: v for k, v in (x.split("=") for x in temp)}
+            categories[current_category].append(temp)
+
+        return yt, title, duration, categories
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
